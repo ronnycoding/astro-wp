2d43798dc281820bbd32c25eec16e72bf4a6efcf
Ronny Freites <ronnyangelo.freites@gmail.com>
2024-08-19 21:27:59 -0600

    add shadcn/ui

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,10 @@
+import { getSdk } from "@/gql/sdk";
 import { GraphQLClient } from "graphql-request";
-import { getSdk } from "../gql/sdk";
 
 const endpoint = import.meta.env.WORDPRESS_API_URL;
+if (!endpoint) {
+  throw new Error("Missing WORDPRESS_API_URL");
+}
 const client = new GraphQLClient(endpoint);
 const sdk = getSdk(client);
 
